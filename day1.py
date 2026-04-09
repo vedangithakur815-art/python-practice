4d01bf5216e8f4ccb4ca335465e99bcafeb03a27
vedangithakur815-art <vedangithakur815@gmail.com>
2026-04-09 23:47:35 +0530

    Add item input and calculation functionality
    Fix typo in print function and add basic item calculation.

--- day1.py
+++ day1.py
@@ -1,1 +1,13 @@
+# first Question
 print("Hello GitHub! My first program")
+
+# Second question
+my_item = input("enter the name of the item")
+quantity = int(input("enter the quantity of the item"))
+price = float(input("enter the price of the item"))
+subtotal = price*quantity
+tax = subtotal*0.05
+total = subtotal + tax 
+print("subtotal",subtotal)
+print("tax(5%)",tax)
+printn("total amount",total)
