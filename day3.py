a9f2fc610c4a40902e543a753e0a4cfc315c6bd4
vedangithakur815-art <vedangithakur815@gmail.com>
2026-05-27 18:21:30 +0530

    Refactor and add methods in day3.py
    Refactor countDigits method and fix logic errors. Add implementations for isPalindrome and subtractProductAndSum methods.

--- day3.py
+++ day3.py
@@ -43,19 +43,69 @@ class Solution:
 sol = Solution()
 print(sol.smallerNumbersThanCurrent([8,1,2,2,3]))
 
-#Question 4(Count the Digits that divide a Number)
-from typing import List
+#Question 4(count the Digits that divide a Number)
 class Solution:
   def countDigits(self, num: int) -> int:
-    temp = num
-    ans = 0
-    while temp>0:
-      r = temp>0:
-      if num%r==0:
-        ans+1
-      temp//=10  
+      temp = num
+      ans = 0
+      while temp>0:
+          r = temp%10
+          if r!=0 and num%r==0:
+              ans+=1
+          temp//=10  
 
-     return ans
+      return ans
+
+s = Solution()
+print(s.countDigits(1212))
     
+#Question 5(Palindrome number)
+class Solution:
+  def isPalindrome(self,x: int) -> bool:
+      temp = x
+      rev = 0
+      
+      while temp>0:
+          r = temp%10
+          temp//=10
+          rev = rev*10+r
 
+      return rev==x
+s = Solution()
+print(s.isPalindrome(121))
+
+#Question 6(Subtract the Product and Sum of Digits of an Integer)
+class Solution:
+  def subtractProductAndSum(self, n: int) -> int:
+      temp = n
+      sum_ = 0
+      product = 1
+
+      while temp>0:
+          r = temp%10
+          product *= r
+          temp//=10
+          sum_+=r
+
+      return product - sum_
+
+s = Solution()
+print(s.subtractProductAndSum(234))
+
+#Question 7()
+from typing import List
+class Solution:
+  def kidsWithCandies(self, candies: List[int], extraCandies: int) -> List[bool]:
+    maxCandies = max(candies)
+    ans = []
+
+    for i in candies:
+        if (i+extraCandies)>=maxCandies:
+            ans.append(True)
+        else:
+            ans.append(False)
+
+    return ans   
+s = Solution()
+print(s.kidsWithCandies([2,3,5,1,3],3))
 
